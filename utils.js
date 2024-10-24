076010ef7d26b3538eba4e5f758dcad67c10c1d2
Paco Valdez <paco@cube.dev>
2024-10-24 08:58:03 -0700

    memoize results of async functions

--- utils.js
+++ utils.js
@@ -2,7 +2,14 @@ async function stall(stallTime = 3000) {
   await new Promise(resolve => setTimeout(resolve, stallTime));
 }
 
+const columnList = {};
+
 async function fetchColumnList(tenantId) {
+  // check if columnList is already present for tenantId in columnList
+  if (columnList[tenantId]) {
+    return columnList[tenantId];
+  }
+  // simulate fetch columnList from DB
   await stall();
   console.log(`Tenant ID ${tenantId}`);
   return {'status1': {}, 'status2': {}, 'status3': {}};
