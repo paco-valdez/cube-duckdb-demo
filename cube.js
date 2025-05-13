2ce7491f3229468c0238f87c334245b01b88816c
julio <julio@cube.dev>
2025-05-13 00:27:50 +0000

    Batch update files

--- cube.js
+++ cube.js
@@ -1,15 +1,12 @@
 
 module.exports = {
-  contextToAppId: ({securityContext}) => `CUBEJS_APP_${securityContext.tenant_id || 'default'}`,
-  // contextToOrchestratorId: ({securityContext}) => `CUBEJS_APP_${securityContext.tenant_id || 'default'}`,
-  // preAggregationsSchema: ({securityContext}) => `pre_aggregations_${securityContext.tenant_id || 'default'}`,
-  scheduledRefreshContexts: async () => {
-    const number_of_tenants = Array.from({length: 20}, (x, i) => i);
-    let tenants = [];
-    for(const element of number_of_tenants){
-      tenants.push({ securityContext: { tenant_id: element }})
+  extendContext: (req) => {
+    console.log(req)
+    return {
+      securityContext: {
+        tenant_id: 1
+      }
     }
-    return tenants;
-  },
+  }
 }
 
